b2b0191a33c241ac67f7acd86a4c20e99cea3ee0
Alex Truong <145624358+AlexTruongCa@users.noreply.github.com>
2024-03-23 01:13:52 -0300

    Home component (cont')
    Pending Book Now button

---
+++ nam-to-ink/src/Components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home-text">
+        <h1>
+          WE TRANSFORM YOUR TATTOO INTO MEMORIES THAT SPEAK LOUDER THAN WORDS
+        </h1>
+        <p>Empowering self-expression through the language of art</p>
+        <button className="btn">BOOK NOW</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
