3271fb1a494bdc3d95523b4752b6c5bdd417ed48
Alex Truong <145624358+AlexTruongCa@users.noreply.github.com>
2024-03-29 22:19:02 -0300

    contact component (pending)

--- nam-to-ink/src/Components/Contact/Contact.jsx
+++ nam-to-ink/src/Components/Contact/Contact.jsx
@@ -2,7 +2,54 @@ import React from "react";
 import "./Contact.css";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="contact-me">
+      <div className="contact-column">
+        <h3>Get in touch</h3>
+        <ul>
+          <li>Cao Ba Nha, Nguyen Cu Trinh Ward, District 1, HCMC</li>
+          <li>089-661-3869</li>
+        </ul>
+      </div>
+      <div className="booking-column">
+        <h3>To start the booking process, please fill in this form</h3>
+        <form action="">
+          <label>Your Name</label>
+          <input
+            className="your-name"
+            type="text"
+            name="name"
+            placeholder="enter your name"
+            required
+          />
+          <label>Email Address</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="enter your e-mail address"
+            required
+          />
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="enter your mobile phone number"
+            required
+          />
+          <label>Tattoo Design Description</label>
+          <input type="text" name="name" placeholder="select an option" />
+          <label>Placement</label>
+          <input type="text" name="name" placeholder="select an option" />
+          <label>Leave me a message</label>
+          <textarea
+            name="message"
+            rows="10"
+            placeholder="give me more details of your idea"
+          ></textarea>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
