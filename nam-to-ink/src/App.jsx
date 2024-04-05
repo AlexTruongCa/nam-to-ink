a85860c32d6e98da762313cc7b6aa54136a290e8
Alex Truong <145624358+AlexTruongCa@users.noreply.github.com>
2024-04-05 12:51:38 -0300

    dataMock for photo gallery

--- nam-to-ink/src/App.jsx
+++ nam-to-ink/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
@@ -13,35 +14,146 @@ import Photobig from "./Components/Photobig/Photobig";
 import photo_1 from "./assets/photo_1.png";
 import photo_4 from "./assets/photo_4.png";
 import photo_5 from "./assets/photo_5.png";
+import photo_3 from "./assets/photo_3.png";
+
+const dataMock = [
+  {
+    id: "17895695668004550",
+    media_type: "IMAGE",
+    media_url: photo_1,
+    username: "jayposiris",
+    timestamp: "2017-08-31T18:10:00+0000",
+  },
+  {
+    id: "17895695668004551",
+    media_type: "IMAGE",
+    media_url: photo_1,
+    username: "jayposiris",
+    timestamp: "2017-08-31T18:10:00+0000",
+  },
+  {
+    id: "17895695668004552",
+    media_type: "IMAGE",
+    media_url: photo_3,
+    username: "jayposiris",
+    timestamp: "2017-08-31T18:10:00+0000",
+  },
+  {
+    id: "17895695668004553",
+    media_type: "IMAGE",
+    media_url: photo_4,
+    username: "jayposiris",
+    timestamp: "2017-08-31T18:10:00+0000",
+  },
+  {
+    id: "17895695668004554",
+    media_type: "IMAGE",
+    media_url: photo_5,
+    username: "jayposiris",
+    timestamp: "2017-08-31T18:10:00+0000",
+  },
+  {
+    id: "17895695668004554",
+    media_type: "IMAGE",
+    media_url: photo_5,
+    username: "jayposiris",
+    timestamp: "2017-08-31T18:10:00+0000",
+  },
+  {
+    id: "17895695668004554",
+    media_type: "IMAGE",
+    media_url: photo_5,
+    username: "jayposiris",
+    timestamp: "2017-08-31T18:10:00+0000",
+  },
+  {
+    id: "17895695668004554",
+    media_type: "IMAGE",
+    media_url: photo_5,
+    username: "jayposiris",
+    timestamp: "2017-08-31T18:10:00+0000",
+  },
+  {
+    id: "17895695668004554",
+    media_type: "IMAGE",
+    media_url: photo_5,
+    username: "jayposiris",
+    timestamp: "2017-08-31T18:10:00+0000",
+  },
+  {
+    id: "17895695668004554",
+    media_type: "IMAGE",
+    media_url: photo_5,
+    username: "jayposiris",
+    timestamp: "2017-08-31T18:10:00+0000",
+  },
+  {
+    id: "17895695668004554",
+    media_type: "IMAGE",
+    media_url: photo_5,
+    username: "jayposiris",
+    timestamp: "2017-08-31T18:10:00+0000",
+  },
+  {
+    id: "17895695668004554",
+    media_type: "IMAGE",
+    media_url: photo_5,
+    username: "jayposiris",
+    timestamp: "2017-08-31T18:10:00+0000",
+  },
+];
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Home />
-      <About />
-      <Title title="MY WORK" />
-      <Portfolio>
-        <Photo className="photo" src={photo_1} />
-        <Photo className="photo" src={photo_1} />
-        <Photo className="photo" src={photo_1} />
-        <Photobig className="photo-big-1" src={photo_4} />
-        <Photobig className="photo-big-2" src={photo_5} />
-        <Photo className="photo" src={photo_1} />
-        <Photo className="photo" src={photo_1} />
-        <Photo className="photo" src={photo_1} />
-        <Photo className="photo" src={photo_1} />
-        <Photo className="photo" src={photo_1} />
-        <Photo className="photo" src={photo_1} />
-        <Photo className="photo" src={photo_1} />
-      </Portfolio>
-      <Title title="CONTACT ME" titleBackground="#f8f0e8" />
-      <Contact>
-        <Info />
-        <Form />
-      </Contact>
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/work" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+        </Routes>
+        <Home />
+        <About />
+        <Title title="MY WORK" />
+        <Portfolio>
+          {dataMock.map((photoObject, index) => {
+            let className;
+            if (index === 3) {
+              className = "photo-big-1";
+            } else if (index === 4) {
+              className = "photo-big-2";
+            } else className = "photo";
+            return (
+              <Photo
+                className={className}
+                src={photoObject.media_url}
+                key={photoObject.id}
+              />
+            );
+          })}
+          {/* <Photo className="photo" src={photo_1} />
+          <Photo className="photo" src={photo_1} />
+          <Photo className="photo" src={photo_1} />
+          <Photobig className="photo-big-1" src={photo_4} />
+          <Photobig className="photo-big-2" src={photo_5} />
+          <Photo className="photo" src={photo_1} />
+          <Photo className="photo" src={photo_1} />
+          <Photo className="photo" src={photo_1} />
+          <Photo className="photo" src={photo_1} />
+          <Photo className="photo" src={photo_1} />
+          <Photo className="photo" src={photo_1} />
+          <Photo className="photo" src={photo_1} /> */}
+        </Portfolio>
+        <Title title="CONTACT ME" titleBackground="#f8f0e8" />
+        <Contact>
+          <Info />
+          <Form />
+        </Contact>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
--- nam-to-ink/src/Components/Navbar/Navbar.jsx
+++ nam-to-ink/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../../assets/logo_color.png";
 
@@ -15,13 +16,69 @@ const Navbar = () => {
     <nav className={`container ${sticky ? "dark-nav" : ""}`}>
       <img src={logo} alt="" className="logo" />
       <ul>
-        <li>HOME</li>
-        <li>ABOUT ME</li>
-        <li>MY WORK</li>
-        <li>CONTACT</li>
+        <li>
+          <Link to="/">HOME</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="/about">ABOUT ME</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="/work">MY WORK</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="/contact">CONTACT</Link>
+        </li>
       </ul>
     </nav>
   );
 };
 
 export default Navbar;
+
+// import React, { useEffect, useState } from "react";
+// import { Link } from "react-scroll";
+// import "./Navbar.css";
+// import logo from "../../assets/logo_color.png";
+
+// const Navbar = () => {
+//   const [sticky, setSticky] = useState(false);
+
+//   useEffect(() => {
+//     window.addEventListener("scroll", () => {
+//       window.scrollY > 200 ? setSticky(true) : setSticky(false);
+//     });
+//   }, []);
+
+//   return (
+//     <nav className={`container ${sticky ? "dark-nav" : ""}`}>
+//       <img src={logo} alt="" className="logo" />
+//       <ul>
+//         <li>
+//           <Link to="home" smooth={true} offset={0} duration={500}>
+//             HOME
+//           </Link>
+//         </li>
+//         <li>
+//           <Link to="about" smooth={true} offset={-150} duration={500}>
+//             ABOUT ME
+//           </Link>
+//         </li>
+//         <li>
+//           <Link to="portfolio" smooth={true} offset={-245} duration={500}>
+//             MY WORK
+//           </Link>
+//         </li>
+//         <li>
+//           <Link to="contact-me" smooth={true} offset={-245} duration={500}>
+//             CONTACT
+//           </Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
