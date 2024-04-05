ed220b4facdd0f76e946dc494d4ceab7623d7059
Alex Truong <145624358+AlexTruongCa@users.noreply.github.com>
2024-04-05 19:48:13 -0300

    Scroll Link
    using react-scroll

--- nam-to-ink/src/App.jsx
+++ nam-to-ink/src/App.jsx
@@ -53,87 +53,86 @@ const dataMock = [
     timestamp: "2017-08-31T18:10:00+0000",
   },
   {
-    id: "17895695668004554",
+    id: "17895695668004559",
     media_type: "IMAGE",
     media_url: photo_5,
     username: "jayposiris",
     timestamp: "2017-08-31T18:10:00+0000",
   },
   {
-    id: "17895695668004554",
+    id: "17895695668004557",
     media_type: "IMAGE",
     media_url: photo_5,
     username: "jayposiris",
     timestamp: "2017-08-31T18:10:00+0000",
   },
   {
-    id: "17895695668004554",
+    id: "17895695668004558",
     media_type: "IMAGE",
     media_url: photo_5,
     username: "jayposiris",
     timestamp: "2017-08-31T18:10:00+0000",
   },
   {
-    id: "17895695668004554",
+    id: "17895695668004512",
     media_type: "IMAGE",
     media_url: photo_5,
     username: "jayposiris",
     timestamp: "2017-08-31T18:10:00+0000",
   },
   {
-    id: "17895695668004554",
+    id: "17895695668004515",
     media_type: "IMAGE",
     media_url: photo_5,
     username: "jayposiris",
     timestamp: "2017-08-31T18:10:00+0000",
   },
   {
-    id: "17895695668004554",
+    id: "17895695668004565",
     media_type: "IMAGE",
     media_url: photo_5,
     username: "jayposiris",
     timestamp: "2017-08-31T18:10:00+0000",
   },
   {
-    id: "17895695668004554",
+    id: "17895695668004523",
     media_type: "IMAGE",
     media_url: photo_5,
     username: "jayposiris",
     timestamp: "2017-08-31T18:10:00+0000",
   },
 ];
-
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/work" component={Portfolio} />
-          <Route path="/contact" component={Contact} />
-        </Routes>
-        <Home />
-        <About />
-        <Title title="MY WORK" />
-        <Portfolio>
-          {dataMock.map((photoObject, index) => {
-            let className;
-            if (index === 3) {
-              className = "photo-big-1";
-            } else if (index === 4) {
-              className = "photo-big-2";
-            } else className = "photo";
-            return (
-              <Photo
-                className={className}
-                src={photoObject.media_url}
-                key={photoObject.id}
-              />
-            );
-          })}
-          {/* <Photo className="photo" src={photo_1} />
+    // <Router>
+    <div>
+      <Navbar />
+      {/* <Routes>
+          <Route path="/" exact Component={Home} />
+          <Route path="/about" Component={About} />
+          <Route path="/work" Component={Portfolio} />
+          <Route path="/contact-me" Component={Contact} />
+        </Routes> */}
+      <Home />
+      <About />
+      <Title title="MY WORK" />
+      <Portfolio>
+        {dataMock.map((photoObject, index) => {
+          let className;
+          if (index === 3) {
+            className = "photo-big-1";
+          } else if (index === 4) {
+            className = "photo-big-2";
+          } else className = "photo";
+          return (
+            <Photo
+              className={className}
+              src={photoObject.media_url}
+              key={photoObject.id}
+            />
+          );
+        })}
+        {/* <Photo className="photo" src={photo_1} />
           <Photo className="photo" src={photo_1} />
           <Photo className="photo" src={photo_1} />
           <Photobig className="photo-big-1" src={photo_4} />
@@ -145,15 +144,15 @@ const App = () => {
           <Photo className="photo" src={photo_1} />
           <Photo className="photo" src={photo_1} />
           <Photo className="photo" src={photo_1} /> */}
-        </Portfolio>
-        <Title title="CONTACT ME" titleBackground="#f8f0e8" />
-        <Contact>
-          <Info />
-          <Form />
-        </Contact>
-        <Footer />
-      </div>
-    </Router>
+      </Portfolio>
+      <Title title="CONTACT ME" titleBackground="#f8f0e8" />
+      <Contact>
+        <Info />
+        <Form />
+      </Contact>
+      <Footer />
+    </div>
+    // </Router>
   );
 };
 
